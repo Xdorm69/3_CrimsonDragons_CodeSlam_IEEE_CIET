23a4629706d6a21c917c608a595adfe2a5ecc640
Amitoj <amitoj.mehta@gmail.com>
2025-11-11 10:33:01 +0530

    base0 code

---
+++ components/Wrappers/MaxWidthWrapper.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MaxWidthWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8">{children}</div>
+  );
+};
+
+export default MaxWidthWrapper;
